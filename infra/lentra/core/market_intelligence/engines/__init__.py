6b226384ab298130863a5657c6f6bde0ff72ddce
flyrum <dev@flyrum.local>
2026-07-13 19:43:53 +0000

    Remove obsolete safe engine wrapper legacy layer

--- infra/lentra/core/market_intelligence/engines/__init__.py
+++ infra/lentra/core/market_intelligence/engines/__init__.py
@@ -3,5 +3,3 @@ from .risk_engine import RiskEngine
 from .signals_engine import SignalsEngine
 from .dedup_engine import DedupEngine
 from .area_engine import AreaEngine
-
-from .safe_engine_wrapper import SafeEngineWrapper
