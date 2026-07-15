d22a820bae667330dd99011b7148cebfbb8ffe27
flyrum <dev@flyrum.local>
2026-07-15 14:27:19 +0000

    Remove dead graph safe hook layer

--- infra/lentra/api/patch_graph_integration.py
+++
@@ -1,9 +0,0 @@
-from lentra.core.market_intelligence.graph.graph_hook import attach_graph_layer
-
-
-def apply_graph_layer(gateway, engines, payload, response):
-    """
-    SAFE wrapper
-    """
-
-    return attach_graph_layer(response, engines, payload)
