3950a2f01ca65038eb0a87ec3603a654dc07b0fe
flyrum <dev@flyrum.local>
2026-07-15 14:49:30 +0000

    Remove legacy SignalsEngine export from market intelligence engines

--- infra/lentra/core/market_intelligence/engines/__init__.py
+++ infra/lentra/core/market_intelligence/engines/__init__.py
@@ -1,5 +1,4 @@
 from .pricing_engine import PricingEngine
 from .risk_engine import RiskEngine
-from .signals_engine import SignalsEngine
 from .dedup_engine import DedupEngine
 from .area_engine import AreaEngine
