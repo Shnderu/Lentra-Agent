63fddeb87b489802d5b46d84855e33b55883e778
flyrum <dev@flyrum.local>
2026-07-16 07:47:11 +0000

    remove deprecated core listing model

--- infra/lentra/core/models/listing.py
+++
@@ -1,9 +0,0 @@
-"""
-LEGACY DEPRECATED
-DO NOT USE
-
-Replaced by:
-- lentra.core.dto.listing_dto.ListingDTO
-"""
-
-raise RuntimeError("Legacy Listing model is deprecated. Use ListingDTO instead.")
